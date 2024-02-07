ddbc9c5657a0ee7bf76e3747b50569f09365d611
shubham <girishubham196@gmail.com>
2024-02-07 09:56:41 +0530

    apis

--- Dropbox/PC/Desktop/estate/api/index.js
+++ Dropbox/PC/Desktop/estate/api/index.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
   .connect(
-    "mongodb+srv://op:op@cluster0.hhigoat.mongodb.net/campstore?authSource=admin&replicaSet=atlas-ar1oxj-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+    "mongodb+srv://op:op@cluster0.hhigoat.mongodb.net/estate?authSource=admin&replicaSet=atlas-ar1oxj-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
   )
   .then(() => {
     console.log("Connected To MongoDb");
@@ -17,9 +18,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
